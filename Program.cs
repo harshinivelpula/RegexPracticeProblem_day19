f9a01544f5c4dcd0b71665e32916bc50bcc171d1
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 12:17:07 +0530

    enter last name with start cap min 3 chars

--- Program.cs
+++ Program.cs
@@ -2,19 +2,36 @@
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    validfirstname validFirstName = new validfirstname();
+        //    Console.WriteLine("enter the name");
+        //    string validfirstname = Console.ReadLine();
+        //    bool value1 = validFirstName.validatefirstname(validfirstname);
+        //    if (value1)
+        //    {
+        //        Console.WriteLine("name is matched");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("name is not matched");
+        //    }
+        //}
         public static void Main(string[] args)
         {
-            validfirstname validFirstName = new validfirstname();
-            Console.WriteLine("enter the name");
-            string validfirstname = Console.ReadLine();
-            bool value1 = validFirstName.validatefirstname(validfirstname);
-            if (value1)
+            validlastname validLastName = new validlastname();
+            Console.WriteLine("please enter the name");
+
+            string validlastname = Console.ReadLine();
+
+            bool value = validLastName.validatelastname(validlastname);
+            if (value)
             {
-                Console.WriteLine("name is matched");
+                Console.WriteLine("Name is matched");
             }
             else
             {
-                Console.WriteLine("name is not matched");
+                Console.WriteLine("Name is not matched");
             }
         }
     }
