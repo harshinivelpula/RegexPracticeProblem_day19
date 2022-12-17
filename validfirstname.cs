3f03ab30ae5046376c6975f1ae637e90c9b4f79e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 12:02:27 +0530

    first name with cap min 3 chars

---
+++ validfirstname.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem19
+{
+    public class validfirstname
+    {
+        public static string REGEX_VALIDFIRSTNAME = (@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public bool validatefirstname(string validfirstname)
+        {
+            return Regex.IsMatch(validfirstname, REGEX_VALIDFIRSTNAME);
+        }
+    }
+}
