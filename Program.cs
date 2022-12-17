a333b794030fa63e55f1c22d5e6067a56646093e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 12:03:48 +0530

    first name with cap min 3 chars

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UserRegistrationProblem19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            validfirstname validFirstName = new validfirstname();
+            Console.WriteLine("enter the name");
+            string validfirstname = Console.ReadLine();
+            bool value1 = validFirstName.validatefirstname(validfirstname);
+            if (value1)
+            {
+                Console.WriteLine("name is matched");
+            }
+            else
+            {
+                Console.WriteLine("name is not matched");
+            }
+        }
+    }
+}
